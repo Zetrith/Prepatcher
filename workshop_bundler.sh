f4e5782ac04d02bec20bdd9f547eff6eec3e8b65
Zetrith <zetrith@gmail.com>
2023-02-26 05:13:03 +0100

    Load before Harmony, add workshop_bundler.sh, update gitignore

---
+++ workshop_bundler.sh
@@ -0,0 +1,13 @@
+cd Source
+dotnet build --configuration Release
+cd ..
+
+rm -rf Prepatcher/
+mkdir -p Prepatcher
+cp -r About Assemblies Prepatcher/
+
+# Zip for Github releases
+rm -f Prepatcher.zip
+zip -r -q Prepatcher.zip Prepatcher
+
+echo "Ok, $PWD/Prepatcher.zip ready for uploading to Workshop"
